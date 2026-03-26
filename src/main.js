ee36ffc53c5b488eec4b107e8b2ce500565d3808
Bouncybull <vinceschoutrop@gmail.com>
2026-03-26 16:33:40 +0100

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,38 @@
+import "./style.scss";
+import * as THREE from 'three';
+
+const canvas = document.querySelector("#experience-canvas");
+const sizes ={
+  width:  window.innerWidth,
+  height: window.innerHeight
+}
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 
+  75, 
+  sizes.width / sizes.height, 
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true });
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+const render = () =>{
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render( scene, camera );
+
+  window.requestAnimationFrame(render);
+}
+
+render();
