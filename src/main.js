1bc8694819993275efe4e1ee2ea93245c3d14bf5
Bouncybull <vinceschoutrop@gmail.com>
2026-06-19 16:52:43 +0200

    Fixed lighting and loading of model

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from "three/examples/jsm/Addons.js";
 import { OutlineEffect } from 'three/addons/effects/OutlineEffect.js';
 
+let CameraPosition1 = new THREE.Vector3(3.499, 33.030, -8.456);
+
 const canvas = document.querySelector("#experience-canvas");
 const sizes ={
   zoom_out:  1.5,
@@ -18,43 +20,77 @@ scene.background = new THREE.Color( 0x66c0ff);
 const camera = new THREE.PerspectiveCamera( 
   75, 
   sizes.width / sizes.height, 
-  0.1,
+  0.001,
   100000
 );
 
 const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-// scene.add( cube );
-
-camera.position.z = 100;
-
+renderer.shadowMap.enabled = true;
 
 const loader = new GLTFLoader();
-loader.load('./models/exportThreejsNoOutlines.gltf', (gltf) => {
+loader.load('./models/exportThreejsFinal.gltf', (gltf) => {
   const appartment = gltf.scene;
+  appartment.position.set(0, 0, 0); 
+  appartment.rotation.y = Math.PI;
+  for(const child of appartment.children)
+  {
+    child.castShadow = true;
+    child.receiveShadow = true;
+    for(const kid of child.children)
+    {
+      kid.castShadow = true;
+      kid.receiveShadow = true;
+    }
+  }
+  appartment.castShadow = true;
+  console.log(appartment);
   scene.add(appartment);
 });
 
-const light = new THREE.AmbientLight( 0x404040, 100 ); // soft white light
+const light = new THREE.AmbientLight( 0xffffff, 1 );
 scene.add( light );
 
-// const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
-// scene.add( directionalLight );
+const directionalLight = new THREE.DirectionalLight( 0xffffff, 3 );
+directionalLight.position.set(  1, 1.75,  1 );
+directionalLight.color.setHSL( 0.1, 1, 0.95 );
+directionalLight.position.multiplyScalar( 30 );
+scene.add( directionalLight );
+
+directionalLight.castShadow = true;
+
+directionalLight.shadow.mapSize.width = 2048;
+directionalLight.shadow.mapSize.height= 2048;
+
+const d = 50;
+
+directionalLight.shadow.camera.left = -d;
+directionalLight.shadow.camera.right = d;
+directionalLight.shadow.camera.top = d;
+directionalLight.shadow.camera.bottom = -d;
+
+directionalLight.shadow.camera.far = 3500;
+directionalLight.shadow.bias = - 0.0001;
+
+const dirLightHelper = new THREE.DirectionalLightHelper( directionalLight, 10 );
+scene.add( dirLightHelper );
 
 // Event Listeners & Functions 
 
-const controls = new OrbitControls( camera, renderer.domElement );
+// const controls = new OrbitControls( camera, renderer.domElement );
 // controls.update() must be called after any manual changes to the camera's transform
-camera.position.set( 0, 20, 100 );
-controls.update();
+// camera.position.set( 0, 20, 100 );
+camera.position.set(-8, 25, 17);
+camera.rotation.x = THREE.MathUtils.degToRad(0);
+camera.rotation.y = THREE.MathUtils.degToRad(0);
+camera.rotation.z = THREE.MathUtils.degToRad(0);
+
+// controls.update();
+
 function animate() {
 	// required if controls.enableDamping or controls.autoRotate are set to true
-	controls.update();
+	// controls.update();
 	renderer.render( scene, camera );
 }
 
@@ -98,12 +134,10 @@ const resizeWindow = () =>{
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 }
 
-const effect = new OutlineEffect( renderer);
+const effect = new OutlineEffect( renderer, { defaultThickness: 0.006});
 
 const render = () =>{
-  // cube.rotation.x += 0.01;
-  // cube.rotation.y += 0.01;
-  
+
   renderer.render( scene, camera );
   effect.render( scene, camera );
   window.requestAnimationFrame(render);
